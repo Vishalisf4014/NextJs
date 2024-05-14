552ab3c7c0e91d2faa373bbf3ca12c859ae32234
Vishalisf4014 <118177824+Vishalisf4014@users.noreply.github.com>
2024-05-14 16:59:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/count-pages": ["./node_modules/mupdf/lib/*.wasm"],
-    },
-  },
 }
 
 module.exports = nextConfig
